1ba5892af34e7dcdd9a1d498a5e7c000ee53e495
murku <downquark@gmail.com>
2024-07-08 21:25:58 +0200

    completed tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+// source: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
+
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread, 
+    time::Duration,
 };
+
+use tutorial_simple_webserver::ThreadPool;
 
 fn main() {
     let port: i32 = 7878;
     let listener: TcpListener = TcpListener::bind(
         format!("127.0.0.1:{}", port)).unwrap();
-    
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
-    
-        handle_connection(stream);
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -20,10 +29,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "sleep.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -34,5 +46,4 @@ fn handle_connection(mut stream: TcpStream) {
     );
 
     stream.write_all(response.as_bytes()).unwrap(); //ToDo: Error handling
-
 }
